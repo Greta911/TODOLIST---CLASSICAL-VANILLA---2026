4f4f671c78da0c3c2b28e4676c061f5e8e10c192
Greta911 <grema20012000@yahoo.it>
2026-05-11 15:49:34 +0200

    Affichage initial des todos

---
+++ app.js
@@ -0,0 +1,28 @@
+
+  //Initialisation d'un localStorage avec des todos
+
+    localStorage.todos = JSON.stringify([
+      { id: 1, content: "Tâche 1", completed: true },
+      { id: 2, content: "Tâche 2", completed: false },
+    ]);
+  //Elements utiles du DOM
+  const todos = JSON.parse(localStorage.todos) || [];
+  const appElement = document.querySelector(".todoapp");
+  const addInputElement = appElement.querySelector(".new-todo");
+  const todosContainer = appElement.querySelector(".todo-list");
+  const notCompletedCountElement = appElement.querySelector(".todo-count");
+
+  //Ajout initial des items dans le DOM
+  todos.forEach(item => {
+    const newItem = document.createElement('div');
+    todosContainer.append(newItem);
+    newItem.outerHTML = `<li data-id="${item.id}" class="${item.completed ? 'completed' : ''}">
+            <div class="view">
+                <input class="toggle" type="checkbox" ${item.completed ? 'checked' : ''} />
+                <label>${item.content}</label>
+                <button class="destroy"></button>
+            </div>
+            <input class="edit" value="${item.content}" />
+          </li>`;
+  })
+
